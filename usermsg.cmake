1e781a3b7edb9e260c0cf0d423f50d88bd91a7bd
Nicu Tofan <nicu.tofan@gmail.com>
2015-03-06 19:17:04 +0200

    debug messages off by default

--- usermsg.cmake
+++ usermsg.cmake
@@ -1,6 +1,6 @@
 
 # enable/disable cmake debug messages related to this pile
-set (USERMSG_DEBUG_MSG ON)
+set (USERMSG_DEBUG_MSG OFF)
 
 # make sure support code is present; no harm
 # in including it twice; the user, however, should have used
