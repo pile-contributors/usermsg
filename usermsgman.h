4da7a447318695afe00f8e2c6876779b95a60bc6
Nicu Tofan <nicu.tofan@gmail.com>
2016-01-04 14:46:58 +0200

    local changes

--- usermsgman.h
+++ usermsgman.h
@@ -1,7 +1,7 @@
 /**
  * @file usermsgman.h
  * @brief Declarations for UserMsgMan class
- * @author 
+ * @author Nicu Tofan <nicu.tofan@gmail.com>
  * @copyright Copyright 2014 piles contributors. All rights reserved.
  * This file is released under the
  * [MIT License](http://opensource.org/licenses/mit-license.html)
